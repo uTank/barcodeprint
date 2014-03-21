9fa59804e31a9827e6453b1a1dbf69c1e892d061
uTank <hongyt2045@163.com>
2014-03-21 16:50:32 +0800

    Create stdint.h

---
+++ stdint.h
@@ -0,0 +1,16 @@
+#ifndef __STDINT_H__
+#define __STDINT_H__
+
+#define uint8_t 		unsigned char 
+#define uint16_t		unsigned short int
+#define uint32_t 		unsigned int
+#define uint64_t 		unsigned long int
+
+#define int8_t 			char
+#define int16_t			short int
+#define int32_t			int
+#define int64_t			long int
+
+#define FILENAME_MAX 	256
+
+#endif /* __STDINT_H__ */
